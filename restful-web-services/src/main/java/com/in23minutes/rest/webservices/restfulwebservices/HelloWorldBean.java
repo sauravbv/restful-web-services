96b92613d3206eb7a67fa2bcff1d9c1af4483ea0
sauravbv <41701284+sauravbv@users.noreply.github.com>
2019-10-15 18:35:02 +0530

    first commit

---
+++ restful-web-services/src/main/java/com/in23minutes/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,22 @@
+package com.in23minutes.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+	
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+	
+	
+}
